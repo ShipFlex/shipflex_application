d6709152e45c6b2191ac07ba5be18c442709d747
saksagan86 <bgunenc0@gmail.com>
2023-03-13 15:42:05 +0100

    Updated main

--- src/main/java/io/github/ShipFlex/shipflex_application/Main.java
+++ src/main/java/io/github/ShipFlex/shipflex_application/Main.java
@@ -5,6 +5,6 @@ import javax.sound.sampled.SourceDataLine;
 class Main {
     public static void main(String args[]) {
 
-        System.out.println("Hello World");
+        System.out.println("berkjaay");
     }
 }
