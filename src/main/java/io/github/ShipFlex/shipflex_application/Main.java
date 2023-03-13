6982fde722523413579ab21be2987c797b30d6cf
Lossttt <lfatmi.ayoub@gmail.com>
2023-03-13 15:35:12 +0100

    Initial commit

---
+++ src/main/java/io/github/ShipFlex/shipflex_application/Main.java
@@ -0,0 +1,10 @@
+package main.java.io.github.ShipFlex.shipflex_application;
+
+import javax.sound.sampled.SourceDataLine;
+
+class Main {
+    public static void main(String args[]) {
+
+        System.out.println("Hello World");
+    }
+}
