3f92f2431a3842698cd821de0a6eadb48df4de67
Lossttt <lfatmi.ayoub@gmail.com>
2023-03-16 21:23:56 +0100

    Updated bedrijf to get rid of duplicate code

--- src/main/java/io/github/ShipFlex/shipflex_application/Bedrijf.java
+++ src/main/java/io/github/ShipFlex/shipflex_application/Bedrijf.java
@@ -1,88 +1,26 @@
 package main.java.io.github.ShipFlex.shipflex_application;
 
-public class Bedrijf implements Printgegevens {
-    private String bedrijfsNaam;
-    private String bedrijfsAdres;
-    private String bedrijfsPostcode;
-    private String bedrijfsPlaats;
-    private String bedrijfsTel;
-    private String bedrijfsWebsite;
-    private String bedrijfsKvk;
+public class Bedrijf extends Klant{
+    private String kvkNummer;
 
     // Constructor
-    public Bedrijf(String bedrijfsNaam, String bedrijfsAdres, String bedrijfsPostcode, String bedrijfsPlaats,
-            String bedrijfsTel, String bedrijfsWebsite, String bedrijfsKvk) {
-        this.bedrijfsNaam = bedrijfsNaam;
-        this.bedrijfsAdres = bedrijfsAdres;
-        this.bedrijfsPostcode = bedrijfsPostcode;
-        this.bedrijfsPlaats = bedrijfsPlaats;
-        this.bedrijfsTel = bedrijfsTel;
-        this.bedrijfsWebsite = bedrijfsWebsite;
-        this.bedrijfsKvk = bedrijfsKvk;
+    public Bedrijf(String naam, String adres, String postcode, String plaats, 
+    String land, String emailadres, String telefoonnummer, String klantnummer, String kvkNummer) {
+        
+        super(naam, adres, postcode, plaats, land, emailadres, telefoonnummer, klantnummer);
+        this.kvkNummer = kvkNummer;
     }
 
-    // Getters & Setters
-    public static String getBedrijfsNaam() {
-        return "MyCompany";
+    // Getter & Setter
+    public String getKvkNummer() {
+        return kvkNummer;
     }
 
-    public void setBedrijfsNaam(String bedrijfsNaam) {
-        this.bedrijfsNaam = bedrijfsNaam;
+    public void setKvkNummer(String kvkNummer) {
+        this.kvkNummer = kvkNummer;
     }
 
-    public static String getBedrijfsAdres() {
-        return "Voorbeeldstraat 3";
-    }
-
-    public void setBedrijfsAdres(String bedrijfsAdres) {
-        this.bedrijfsAdres = bedrijfsAdres;
-    }
-
-    public static String getBedrijfsPostcode() {
-        return "0101AB";
-    }
-
-    public void setBedrijfsPostcode(String bedrijfsPostcode) {
-        this.bedrijfsPostcode = bedrijfsPostcode;
-    }
-
-    public static String getBedrijfsPlaats() {
-        return "Den Haag";
-    }
-
-    public void setBedrijfsPlaats(String bedrijfsPlaats) {
-        this.bedrijfsPlaats = bedrijfsPlaats;
-    }
-
-    public static String getBedrijfsTel() {
-        return "064206910";
-    }
-
-    public void setBedrijfsTel(String bedrijfsTel) {
-        this.bedrijfsTel = bedrijfsTel;
-    }
-
-    public static String getBedrijfsWebsite() {
-        return "MyCompany.org";
-    }
-
-    public void setBedrijfsWebsite(String bedrijfsWebsite) {
-        this.bedrijfsWebsite = bedrijfsWebsite;
-    }
-
-    public static String getBedrijfsKvk() {
-        return "0102 0304";
-    }
-
-    public void setBedrijfsKvk(String bedrijfsKvk) {
-        this.bedrijfsKvk = bedrijfsKvk;
-    }
-
-    @Override
-    public void Printgegevens() {
-        System.out.println("\n" + "~~ Bedrijfsgegevens ~~");
-        System.out.print(getBedrijfsNaam() + "\n" + getBedrijfsAdres() + "" + getBedrijfsPostcode()
-                + "\n" + getBedrijfsPlaats() + "\n" + getBedrijfsTel() + "\n" + getBedrijfsWebsite() + "\n" +
-                getBedrijfsKvk());
+    public String toString() {
+        return super.toString() + "\n KVK: " + kvkNummer;
     }
 }
