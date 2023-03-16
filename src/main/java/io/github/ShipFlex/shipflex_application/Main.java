bb881f81029195a239d78852c8f7b0e1a0de0463
Lossttt <lfatmi.ayoub@gmail.com>
2023-03-16 21:25:10 +0100

    Added new objects to call different method classes

--- src/main/java/io/github/ShipFlex/shipflex_application/Main.java
+++ src/main/java/io/github/ShipFlex/shipflex_application/Main.java
@@ -2,17 +2,11 @@ package main.java.io.github.ShipFlex.shipflex_application;
 
 import java.util.Scanner;
 
-import javax.sound.sampled.SourceDataLine;
-
 class Main {
     public static void main(String args[]) {
-        Scanner input = new Scanner(System.in);
 
-        Klant k = new Klant(null, null, null, null, null, null, null);
-        Bedrijf b = new Bedrijf(null, null, null, null, null, null, null);
-
-        k.Printgegevens();
-        b.Printgegevens();
-
+    Klant klant = new KlantInvoer().getKlantGegevens();
+    Offerte offerte = new Offerte(klant);
+    offerte.printOfferte();
     }
 }
