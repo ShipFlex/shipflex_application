288f10842d183cf3c7070f1c45b70d0a4547fae1
Lossttt <lfatmi.ayoub@gmail.com>
2023-03-17 21:56:26 +0100

    Added object that calles printOfferteToFile function

--- src/main/java/io/github/ShipFlex/shipflex_application/Main.java
+++ src/main/java/io/github/ShipFlex/shipflex_application/Main.java
@@ -1,12 +1,13 @@
 package main.java.io.github.ShipFlex.shipflex_application;
 
-import java.util.Scanner;
+import java.io.IOException;
 
 class Main {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
 
     Klant klant = new KlantInvoer().getKlantGegevens();
     Offerte offerte = new Offerte(klant);
     offerte.printOfferte();
+    offerte.printOfferteToFile("B:/Java/shipflex/output_offerte.txt");
     }
 }
