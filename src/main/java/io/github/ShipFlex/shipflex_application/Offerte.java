87ced642d7d5db4db47fc9a92e4223f3bc15d73d
Lossttt <lfatmi.ayoub@gmail.com>
2023-03-22 20:42:08 +0100

    Removed

--- src/main/java/io/github/ShipFlex/shipflex_application/Offerte.java
+++ src/main/java/io/github/ShipFlex/shipflex_application/Offerte.java
@@ -44,49 +44,5 @@ public class Offerte {
             System.out.println(particulier.getKlantnummer());
         }
     }
-
-    public void printOfferteToFile(String filename) throws IOException {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
-
-            // Definieerd de Lijst van variabelen die geprint worden naar de externe file
-            List<String> variables = new ArrayList<>();
-            variables.add(klant.getNaam());
-            variables.add(klant.getAdres());
-            variables.add(klant.getPostcode());
-            variables.add(klant.getPlaats());
-            variables.add(klant.getLand());
-            variables.add(klant.getEmailadres());
-            variables.add(klant.getTelefoonnummer());
-
-            // Voegt het kvkNummer of klantnummer toe aan de List gebasseerd op het type klant
-            if (klant instanceof Bedrijf) {
-                Bedrijf bedrijf = (Bedrijf) klant;
-                variables.add(bedrijf.getKvkNummer());
-            } else if (klant instanceof Particulier) {
-                Particulier particulier = (Particulier) klant;
-                variables.add(particulier.getKlantnummer());
-            }
-
-            writer.write("---. Bedrijfsgegevens .--- \n");
-            writer.write("MyCompany \n" + "Voorbeeldstraat 3 " + "0101AB \n" +
-                    "Den Haag \n" + "MyCompany.org" + " \n 0102 0304 \n");
-
-            writer.write("---. Klantgegevens .--- \n");
-
-            // Write the variables to the file using a enhanced for loop
-            for (String variable : variables) {
-                writer.write(variable + "\n");
-            }
-
-            writer.flush();
-            writer.close();
-
-        }
-        catch (IOException e)
-        {
-            System.err.println("Failed to write file: " + e.getMessage());
-            System.exit(1);
-        }
-    }
 }
 
