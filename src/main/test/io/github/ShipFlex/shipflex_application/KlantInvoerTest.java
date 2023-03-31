142f7ccb47a3f82c0cc1f19625f633114b48e7c8
Lossttt <lfatmi.ayoub@gmail.com>
2023-03-31 18:26:14 +0200

    Added test root folder

---
+++ src/main/test/io/github/ShipFlex/shipflex_application/KlantInvoerTest.java
@@ -0,0 +1,10 @@
+package main.test.io.github.ShipFlex.shipflex_application;
+
+import org.junit.Test;
+
+public class KlantInvoerTest {
+    @Test
+    public void testGetKlantGegevens() {
+
+    }
+}
